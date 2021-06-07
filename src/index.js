ff2dd7adfc71fb1c6df570c20a8ca4d79e132024
Patricio Fernandez Ionadi <patricio.f.i@live.com>
2021-06-07 20:18:59 -0300

    c-

--- src/index.js
+++ src/index.js
@@ -3,43 +3,36 @@ require("./connection")
 const express = require("express")
 const app = express()
 const cors = require("cors")
-
 // ---------------------------------------------------------------------
 app.use(cors())
 app.use(express.json())
-
 // ---------------------------------------------------------------------
-// MODELS
-// const Info = require("./models/info")
-// const User = require("./models/user")
-
-// ---------------------------------------------------------------------
-// Router
+// CONTROLLERS
 const infoRouter = require("./controllers/info")
-const sucursalesRouter = require("./controllers/sucursales")
+const sucursalesRouter = require("./controllers/sucursales.controller")
 const sesionRouter = require("./controllers/sesion.controller")
 const usersRouter = require("./controllers/users.controller")
-
 // ---------------------------------------------------------------------
+// ROUTES
+
 // All Info
 app.use("/api", infoRouter)
-// ---------------------------------------------------------------------
+// ----------------------------------
 // Sucursales
 app.use("/api/sucursales", sucursalesRouter)
-// ---------------------------------------------------------------------
+// ----------------------------------
 // Sesion
 app.use("/sesion", sesionRouter)
-// ---------------------------------------------------------------------
+// ----------------------------------
 // Users
 app.use("/api/users", usersRouter)
 
 // ---------------------------------------------------------------------
+// ERROR HANDLING
 app.use((err, req, res, next) => {
 	console.log(err)
 	res.status(404).send(err)
 })
 
 const PORT = process.env.PORT
-app.listen(PORT, () => {
-	console.log(`Server running on port ${PORT}`)
-})
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
