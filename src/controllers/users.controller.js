a7d932d193bbcacde93d09f2b6ffe424adbccb8c
Patricio Fernandez Ionadi <patricio.f.i@live.com>
2021-06-07 19:30:37 -0300

    users controller to promises

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -1,49 +1,53 @@
 const usersRouter = require("express").Router()
 const { User, UserCreation } = require("../models/user")
 
-usersRouter.get("/", async (req, res) => {
-	const allUsers = await User.find({})
-	res.json(allUsers)
+usersRouter.get("/", (req, res, next) => {
+	User.find({})
+		.then((allUsers) => res.json(allUsers))
+		.catch(next)
 })
 
-usersRouter.post("/admin/control/edit", async (req, res) => {
+usersRouter.post("/admin/control/edit", (req, res, next) => {
 	const userIdToChange = req.body.userToChange._id
 	const newValues = req.body.newValues // {...nuevos valores (existentes)}
-	try {
-		// actualizar valores existentes de un usuario-------------------------
-		// await User.findOneAndUpdate({ _id: userIdToChange }, newValues, {
-		// 	new: true,
-		// 	upsert: true,
-		// })
-		// --------------------------------------------------------------------
-		// actualiza o crea nuevos valores
-		await User.findByIdAndUpdate(userIdToChange, newValues)
+	// actualizar valores existentes de un usuario-------------------------
+	// User.findOneAndUpdate({ _id: userIdToChange }, newValues, {
+	// 	new: true,
+	// 	upsert: true,
+	// })
+	// --------------------------------------------------------------------
+	// actualiza o crea nuevos valores
 
-		res.status(201).end()
-	} catch (e) {
-		res.status(304)
-	}
+	User.findByIdAndUpdate(userIdToChange, newValues)
+		.then(() => res.status(201).end())
+		.catch((e) => {
+			res.status(304)
+			next(e)
+		})
 })
 
-usersRouter.post("/register", async (req, res) => {
+usersRouter.post("/register", (req, res, next) => {
 	const userSolicitud = req.body
 	console.log(userSolicitud)
 
-	const [isExistentUser] = await User.find({ email: userSolicitud.email })
+	User.find({ email: userSolicitud.email }).then((isExistentUser) => {
+		if ([isExistentUser]) {
+			return res.status(406)
+		} else {
+			const userToAdd = new UserCreation(userSolicitud)
 
-	if (isExistentUser) {
-		return res.status(406)
-	} else {
-		const userToAdd = new UserCreation(userSolicitud)
-
-		const savedUserToAdd = await userToAdd.save()
-		res.status(201).json(savedUserToAdd).end()
-	}
+			userToAdd
+				.save()
+				.then((savedUserToAdd) => res.status(201).json(savedUserToAdd).end())
+				.catch(next)
+		}
+	})
 })
 
-usersRouter.get("/register", async (req, res) => {
-	const pendingUsers = await UserCreation.find({})
-	res.json(pendingUsers)
+usersRouter.get("/register", (req, res, next) => {
+	UserCreation.find({})
+		.then((pendingUsers) => res.json(pendingUsers))
+		.catch(next)
 })
 
 module.exports = usersRouter
